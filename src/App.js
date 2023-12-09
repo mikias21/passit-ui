08e1f1b241c304be617cd15921042bec18b73cf9
mikias21 <mikiasmesfinA0123@gmail.com>
2023-12-10 04:19:05 +0800

    Worked on forgot password page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // Pages
-import SignupPage from "./pages/SignupPage/SignupPage";
-import SigninPage from "./pages/SigninPage/SigninPage";
+import SignupPage from "./pages/Signup/SignupPage";
+import SigninPage from "./pages/Signin/SigninPage";
 import VerifyAccount from "./pages/VerifyAccount/VerifyAccount";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import ActivateAccount from "./pages/ActivateAccount/ActivateAccount";
 
 import "./App.css";
@@ -12,7 +13,8 @@ function App() {
       {/* <SignupPage /> */}
       {/* <SigninPage /> */}
       {/* <ActivateAccount /> */}
-      <VerifyAccount />
+      {/* <VerifyAccount /> */}
+      <ForgotPassword />
     </div>
   );
 }
---
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,46 @@
+import FormHeader from "../../components/formComponents/FormHeader";
+import EmailInput from "../../components/formComponents/EmailInput";
+import FormButton from "../../components/formComponents/FormButton";
+
+const ForgotPassword = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="mx-auto p-8 md:flex md:space-x-20">
+        <div className="hidden md:block md:w-1/2 md:pr-8">
+          <div className="mb-8">
+            <h2 className="text-3xl font-extrabold text-secondary mb-4 font-body text-center">
+              Welcome to <span className="text-blue">passit</span>
+            </h2>
+            <p className="text-gray-600 font-body text-center">
+              store passwords securely and access whenever needed.
+            </p>
+          </div>
+          <img src="/images/7070629_3293466.svg" alt="" className="w-full" />
+        </div>
+
+        <div className="w-full md:w-1/2 font-body">
+          <FormHeader
+            title="Forgot password to your password"
+            info="Use your email to reset your passit account password."
+          />
+          <EmailInput />
+          <FormButton title="Send email" />
+          <p className="mt-10 text-gray-600 text-sm">
+            Create Account?{"  "}
+            <a href="google" className="text-blue font-semibold">
+              Sign up
+            </a>
+          </p>
+          <p className="mt-2 text-gray-600 text-sm">
+            Already Have an Account?{"  "}
+            <a href="google" className="text-blue font-semibold">
+              Sign in
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
