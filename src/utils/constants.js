2361a81705c5bd5b0d2902bc5fe13369bc58e8b0
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-09 05:42:23 +0800

    Minor fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const constants = {
   API: "http://localhost:5000",
-  RELEASE_API: "https://passit-api.onrender.com",
+  // RELEASE_API: "https://passit-api.onrender.com",
+  RELEASE_API: "https://sea-lion-app-2tsvx.ondigitalocean.app/",
   RELEASE: true,
 };
 
