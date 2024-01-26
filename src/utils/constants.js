6397b420866c1189428c3bdbcaa267f387df3d7a
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-27 05:41:42 +0800

    release mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const constants = {
   API: "http://localhost:5000",
   // RELEASE_API: "https://passit-api.onrender.com",
   RELEASE_API: "https://sea-lion-app-2tsvx.ondigitalocean.app",
-  RELEASE: false,
+  RELEASE: true,
 };
 
 export default constants;
