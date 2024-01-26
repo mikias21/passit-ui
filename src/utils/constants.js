176a6059976f5763a5b8741917c52ff8350069e9
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-27 00:21:18 +0800

    Working on localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ const constants = {
   API: "http://localhost:5000",
   // RELEASE_API: "https://passit-api.onrender.com",
   RELEASE_API: "https://sea-lion-app-2tsvx.ondigitalocean.app",
-  RELEASE: true,
+  RELEASE: false,
 };
 
 export default constants;
