7c6d83e9d5f3da2412224d0e5607c4bb594803a3
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-30 05:12:13 +0800

    Updated API end point

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 const constants = {
   API: "http://localhost:5000",
   // RELEASE_API: "https://passit-api.onrender.com",
-  RELEASE_API: "https://sea-lion-app-2tsvx.ondigitalocean.app",
+  // RELEASE_API: "https://sea-lion-app-2tsvx.ondigitalocean.app",
+  RELEASE_API: "http://3.83.121.237",
   RELEASE: true,
 };
 
